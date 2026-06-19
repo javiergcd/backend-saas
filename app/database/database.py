00040b7d602aef89c85f456c3e3e1accc7ccb843
Javier Gustavo Corrales Delgadillo <gustavo.11cd@gmail.com>
2026-06-19 08:31:24 -0400

    feat: dockerize FastAPI application

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 
 DATABASE_URL = (
-    "postgresql+psycopg://postgres:admin@localhost:5432/backend_saas"
+    "postgresql+psycopg://postgres:admin@host.docker.internal:5432/backend_saas"
+    # "postgresql+psycopg://postgres:admin@localhost:5432/backend_saas"       para correr de manera local
+    # "postgresql+psycopg://postgres:admin@host.docker.internal:5432/backend_saas"     para correr de con Docker Compose en Windows
 )
 
 engine = create_engine(DATABASE_URL)
