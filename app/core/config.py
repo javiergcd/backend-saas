39a456afef0f582b2ab7f722241ab4aff6b228a1
Javier Gustavo Corrales Delgadillo <gustavo.11cd@gmail.com>
2026-06-20 08:23:36 -0400

    feat: add environment-based configuration management

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,9 @@
-SECRET_KEY = "mi_clave_super_secreta"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int (os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
--- app/database/database.py
+++ app/database/database.py
@@ -1,16 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = (
-    "postgresql+psycopg://postgres:admin@host.docker.internal:5432/backend_saas"
-    # "postgresql+psycopg://postgres:admin@localhost:5432/backend_saas"       para correr de manera local
-    # "postgresql+psycopg://postgres:admin@host.docker.internal:5432/backend_saas"     para correr de con Docker Compose en Windows
-)
+import os
+
+from app.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
